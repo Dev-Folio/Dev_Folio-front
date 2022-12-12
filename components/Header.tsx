db039342d902a6659ff872e6d28d407b203b971b
UniM0cha <solst_ice@naver.com>
2022-12-12 23:16:06 +0900

    1212 - 헤더 만드는중

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Container, Navbar, NavDropdown } from 'react-bootstrap';
+import styles from '../styles/Header.module.scss';
+
+export default function Header() {
+  return (
+    <Navbar bg="light">
+      <Container>
+        <Navbar.Brand>
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={160} height={40} />
+          </Link>
+        </Navbar.Brand>
+        <div className={styles.profile_wrapper}>
+          <Image src="/logo.png" alt="profile_image" width={20} height={20} />
+          <NavDropdown title="이정윤" align="end">
+            <NavDropdown.Item>마이페이지</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item>로그인</NavDropdown.Item>
+            <NavDropdown.Item>로그아웃</NavDropdown.Item>
+          </NavDropdown>
+        </div>
+      </Container>
+    </Navbar>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MemberProvider } from '../context/MemberProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
