e523c0e72f66703847b59c0322c1cf51fa8515a2
MinJu-A <pinokoiandmj@naver.com>
2022-12-13 02:03:35 +0900

    12-12 그냥 틀만 만들었음

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,3 +25,5 @@ export default function Header() {
     </Navbar>
   );
 }
+
+
---
+++ pages/resetpw.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function resetpw() {
+  return (
+    <div>비밀번호 재설정</div>
+  )
+}
