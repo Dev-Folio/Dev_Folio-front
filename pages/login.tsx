9ac54c1f3e8a93dc4245b038fa22647e7a91a4e2
MinJu-A <pinokoiandmj@naver.com>
2022-12-14 18:51:42 +0900

    뭔가를 뚝딱뚝딱만들어보고있습니다 걍 틀만 만들었어요 틀만

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,52 +1,59 @@
 import Header from "../components/Header";
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function login() {
-
   return (
-    <div
-    className="body"
-    style={{
-        display: 'flex',
-        justifyContent : 'center',
-        alignItems : 'center',
-        width : '100%',
-        height: '100vh'
-    }}
-    >
-        <div
+    <div>
+      <div>
+        <Header />
+      </div>
+      <div
+        className='body'
         style={{
-            verticalAlign :'center'
-        }}>
-            <h1><b>로그인</b></h1>
-            <div>
-                <div
-                style={{
-                    display :'flex',
-                    flexDirection :'column',
-                    width : '300px',
-                    verticalAlign : 'middle'
-                }}
-                >
-                    <label className="font-white">ID</label>
-                        <input type="id" id="id" />
-                        <label className="font-white">Password</label>
-                        <input type="password" id="pw" />
-                        <br />
-                      
-                        <Button variant="primary">Login</Button>
-                </div>
-                <div>
-                    <u><a href="signup">회원가입</a></u>
-                    <u><a href="resetpw">비밀번호 재설정</a></u>
-                </div>
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            verticalAlign: "center",
+          }}
+        >
+          <h1>
+            <b>로그인</b>
+          </h1>
+          <div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "300px",
+                verticalAlign: "middle",
+              }}
+            >
+              <label className='font-white'>ID</label>
+              <input type='id' id='id' />
+              <label className='font-white'>Password</label>
+              <input type='password' id='pw' />
+              <br />
+
+              <Button variant='primary'>Login</Button>
             </div>
-
+            <div>
+              <u>
+                <a href='signup'>회원가입</a>
+              </u>
+              <u>
+                <a href='resetpw'>비밀번호 재설정</a>
+              </u>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    
-  )
+  );
 }
-
-
