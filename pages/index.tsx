eb19f9620a8381198941c02b6489f096f1c35c60
MinJu-A <pinokoiandmj@naver.com>
2022-12-14 21:37:43 +0900

    메인 대충 이렇게 할 거라고 틀만 잡았음

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
+import Card from "../components/Card";
+import { Button, Nav } from "react-bootstrap";
 
 export default function Home() {
   return (
@@ -21,22 +23,44 @@ export default function Home() {
       >
         <div>
           <div className='searchArea'>
-            <input type='text' />
+            <nav className='navbar bg-light'>
+              <div className='container'>
+                <a className='navbar-brand' href='#'>
+                  <Image
+                    src='/public/logo.png'
+                    alt='image'
+                    width='30'
+                    height='24'
+                  ></Image>
+                </a>
+                <Button
+                  className='btn btn-secondary'
+                  style={{ float: "right" }}
+                >
+                  검색하기
+                </Button>
+              </div>
+            </nav>
           </div>
           <br />
           <div className='poupularProject'>
             <h2>
-              <b>최근 업로드 된 프로젝트</b>
+              <b>가장 인기있는 프로젝트</b>
+              <Button style={{ float: "right", marginLeft: "20px;" }}>
+                더보기
+              </Button>
             </h2>
             <hr />
-            {/* 카드 캐러셀은 컴포넌트 만들면 추가 예정 */}
           </div>
           <div className='recentProject'>
             <h2>
               <b>최근 업로드 된 프로젝트</b>
+              <Button style={{ float: "right", marginLeft: "20px;" }}>
+                더보기
+              </Button>
             </h2>
+
             <hr />
-            {/* 카드 캐러셀은 컴포넌트 만들면 추가 예정 */}
           </div>
         </div>
       </div>
