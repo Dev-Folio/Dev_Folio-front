821149a438e2246f9926874a404ea0b6c0d6d0f9
MinJu-A <pinokoiandmj@naver.com>
2022-12-15 13:25:47 +0900

    글 작성 수정 폼 태그 씌우기

--- pages/new.tsx
+++ pages/new.tsx
@@ -18,58 +18,75 @@ export default function NewProject() {
     <>
       <Header />
       <div className={styles.container}>
-        <div className={styles.flextLayout}>
-          <Image
-            className={styles.imageMargin}
-            src='/sample_image.png'
-            alt='logo'
-            width={250}
-            height={320}
-            priority
-          />
-          <div>
-            <label>
-              <b>프로젝트 이름</b>
-            </label>
-            <br />
-            <input type='text' id='name' />
-            <br />
-            <label>
-              <b>함께한 사람</b>
-            </label>
-            {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
-            <input type='text' id='name' className={styles.memberTag} />
-            <br />
-            <label>
-              <b>프로젝트 기간</b>
-            </label>
-            <br />
-            <input type='date' id='startDate' /> -{" "}
-            <input type='date' id='finishDate' />
+        <Form>
+          <div className={styles.flextLayout}>
+            <Image
+              className={styles.imageMargin}
+              src='/sample_image.png'
+              alt='logo'
+              width={250}
+              height={320}
+              priority
+            />
+
+            <div>
+              <Form.Group>
+                <label>
+                  <b>프로젝트 이름</b>
+                </label>
+                <br />
+                <Form.Control type='text' />
+              </Form.Group>
+              <br />
+              <Form.Group>
+                <label>
+                  <b>함께한 사람</b>
+                </label>
+                {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
+                <Form.Control type='text' className={styles.memberTag} />
+              </Form.Group>
+              <br />
+              <Form.Group>
+                <label>
+                  <b>프로젝트 기간</b>
+                </label>
+                <br />
+                <Form.Control type='date' placeholder='시작일' /> -
+                <Form.Control type='date' placeholder='마감일' />
+              </Form.Group>
+            </div>
           </div>
-        </div>
-        <div className={styles.blockLayout}>
-          <label>
-            <b>기술스택</b>
-          </label>
-          {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
-          <input type='text' id='name' className={styles.memberTag} />
-          <label>
-            <b>깃허브 주소</b>
-          </label>
-          <br />
-          <input type='url' id='giturl' />
-          <br />
-          <label>
-            <b>프로젝트 설명</b>
-          </label>
-          <br />
-          <textarea></textarea>
-        </div>
-        <>
-          <ToastEditor />
-        </>
-        <Button> 등록하기 </Button>
+          <div className={styles.blockLayout}>
+            <Form.Group>
+              <label>
+                <b>기술스택</b>
+              </label>
+              {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
+              <Form.Control type='text' className={styles.memberTag} />
+            </Form.Group>
+            <Form.Group>
+              <label>
+                <b>깃허브 주소</b>
+              </label>
+              <br />
+              <Form.Control type='url' />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <label>
+                <b>프로젝트 설명</b>
+              </label>
+              <br />
+              <Form.Control type='textarea' />
+            </Form.Group>
+          </div>
+          <>
+            <ToastEditor />
+          </>
+          <Button variant='primary' type='submit'>
+            등록하기
+          </Button>
+        </Form>
       </div>
     </>
   );
--- pages/project/[projectId]/edit.tsx
+++ pages/project/[projectId]/edit.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from "react";
-import { Button } from "react-bootstrap";
-import Header from "../../../components/Header";
+import React, { useRef, useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import Header from "../components/Header";
 import styles from "../styles/NewProject.module.scss";
 
 // image
@@ -10,69 +10,83 @@ import Image from "next/image";
 import dynamic from "next/dynamic";
 
 export default function NewProject() {
-  const ToastEditor = dynamic(
-    () => import("../../../components/ToastEditorEdit"),
-    {
-      ssr: false,
-    }
-  );
+  const ToastEditor = dynamic(() => import("../components/ToastEditor"), {
+    ssr: false,
+  });
+
   return (
     <>
       <Header />
       <div className={styles.container}>
-        <div className={styles.flextLayout}>
-          <Image
-            className={styles.imageMargin}
-            src='/sample_image.png'
-            alt='logo'
-            width={250}
-            height={320}
-            priority
-          />
-          <div>
-            <label>
-              <b>프로젝트 이름</b>
-            </label>
-            <br />
-            <input type='text' id='name' />
-            <br />
-            <label>
-              <b>함께한 사람</b>
-            </label>
-            {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
-            <input type='text' id='name' className={styles.memberTag} />
-            <br />
-            <label>
-              <b>프로젝트 기간</b>
-            </label>
-            <br />
-            <input type='date' id='startDate' /> -{" "}
-            <input type='date' id='finishDate' />
-          </div>
-        </div>
+        <Form>
+          <div className={styles.flextLayout}>
+            <Image
+              className={styles.imageMargin}
+              src='/sample_image.png'
+              alt='logo'
+              width={250}
+              height={320}
+              priority
+            />
 
-        <div className={styles.blockLayout}>
-          <label>
-            <b>기술스택</b>
-          </label>
-          {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
-          <input type='text' id='name' className={styles.memberTag} />
-          <label>
-            <b>깃허브 주소</b>
-          </label>
-          <br />
-          <input type='url' id='giturl' />
-          <br />
-          <label>
-            <b>프로젝트 설명</b>
-          </label>
-          <br />
-          <textarea></textarea>
-        </div>
-        <>
-          <ToastEditor />
-        </>
-        <Button> 등록하기 </Button>
+            <div>
+              <Form.Group>
+                <label>
+                  <b>프로젝트 이름</b>
+                </label>
+                <br />
+                <Form.Control type='text' />
+              </Form.Group>
+              <br />
+              <Form.Group>
+                <label>
+                  <b>함께한 사람</b>
+                </label>
+                {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
+                <Form.Control type='text' className={styles.memberTag} />
+              </Form.Group>
+              <br />
+              <Form.Group>
+                <label>
+                  <b>프로젝트 기간</b>
+                </label>
+                <br />
+                <Form.Control type='date' placeholder='시작일' /> -
+                <Form.Control type='date' placeholder='마감일' />
+              </Form.Group>
+            </div>
+          </div>
+          <div className={styles.blockLayout}>
+            <Form.Group>
+              <label>
+                <b>기술스택</b>
+              </label>
+              {/* text가 써지는 순간 검색이 작동됨. 그 때 바로 드롭다운이 보여지게 해야 함 */}
+              <Form.Control type='text' className={styles.memberTag} />
+            </Form.Group>
+            <Form.Group>
+              <label>
+                <b>깃허브 주소</b>
+              </label>
+              <br />
+              <Form.Control type='url' />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <label>
+                <b>프로젝트 설명</b>
+              </label>
+              <br />
+              <Form.Control type='textarea' />
+            </Form.Group>
+          </div>
+          <>
+            <ToastEditor />
+          </>
+          <Button variant='primary' type='submit'>
+            등록하기
+          </Button>
+        </Form>
       </div>
     </>
   );
